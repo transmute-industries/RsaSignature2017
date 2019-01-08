2abdbd33608259bf256d2c80d9f1794dcd14b2b6
Orie Steele <orie@ohs.io>
2019-01-08 11:57:10 -0600

    update structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-const RsaSignature2017 = require("./RsaSignature2017");
+const crypto = require("crypto");
 
-module.exports = RsaSignature2017;
+const generateRSAKeypair = require("generate-rsa-keypair");
+
+const { createVerifyData } = require("./common");
+
+const sign = async ({ data, creator, domain, created, privateKey }) => {
+  const options = {
+    type: "RsaSignature2017",
+    creator,
+    domain,
+    nonce: crypto.randomBytes(16).toString("hex"),
+    created: created || new Date().toISOString()
+  };
+  if (!domain) {
+    delete options.domain;
+  }
+  const toBeSigned = await createVerifyData(data, options);
+  const signer = crypto.createSign("sha256");
+  signer.update(toBeSigned);
+  signer.end();
+  const signature = signer.sign(privateKey);
+  return {
+    ...data,
+    signature: {
+      ...options,
+      signatureValue: signature.toString("base64")
+    }
+  };
+};
+
+const verify = async ({ data, publicKey }) => {
+  const toBeSigned = await createVerifyData(data, data.signature);
+  const verifier = crypto.createVerify("sha256");
+  verifier.update(toBeSigned);
+  return verifier.verify(publicKey, data.signature.signatureValue, "base64");
+};
+
+module.exports = {
+  sign,
+  verify,
+  generateRSAKeypair
+};
